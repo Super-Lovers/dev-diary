14773f629fed1c944eb36da825098d3d5b81adc6
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-11-16 19:58:36 +0100

    Updated status format, new fortunes

--- scripts/theme.js
+++ scripts/theme.js
@@ -39,9 +39,9 @@ const light_code = '#000000';
 const light_card = '#ffffff';
 const light_btn_background = '#f8f9fa';
 
-const light_status_background_color = '#d4edda';
-const light_status_border_color = '#c3e6cb';
-const light_status_text_color = '#155724';
+// const light_status_background_color = '#d4edda';
+// const light_status_border_color = '#c3e6cb';
+// const light_status_text_color = '#155724';
 
 // Dark Theme
 const dark_background = '#181A1B';
@@ -66,8 +66,8 @@ const dark_code = '#ffffff';
 const dark_card = '#1D1F20';
 const dark_btn_background = '#181A1B';
 
-const dark_status_color = '#ffd700';
-const dark_status_background_color = 'transparent';
+// const dark_status_color = '#ffd700';
+// const dark_status_background_color = 'transparent';
 
 // ****************************************
 
@@ -152,12 +152,12 @@ function updateElementClassColorTo(element, color) {
 	}
 }
 
-function updateElementClassBorderColorTo(element, color) {
-	const elements = document.getElementsByClassName(element);
-	for (let i = 0; i < elements.length; i++) {
-		elements[i].style.borderColor = color;
-	}
-}
+// function updateElementClassBorderColorTo(element, color) {
+// 	const elements = document.getElementsByClassName(element);
+// 	for (let i = 0; i < elements.length; i++) {
+// 		elements[i].style.borderColor = color;
+// 	}
+// }
 
 function updateElementClassBackgroundImageTo(element, value) {
 	const elements = document.getElementsByClassName(element);
@@ -168,8 +168,8 @@ function updateElementClassBackgroundImageTo(element, value) {
 
 function updateVisuals() {
 	if (currentTheme == 'dark') {
-		toggleElementByClassName('portrait', true);
-		toggleElementByClassName('portrait-video', false);
+		// toggleElementByClassName('portrait', true);
+		// toggleElementByClassName('portrait-video', false);
 
 		updateElementColorTo('h1', dark_h1);
 		updateElementColorTo('h3', dark_h3);
@@ -204,13 +204,13 @@ function updateVisuals() {
 		updateElementClassColorTo('profession-icon', dark_a);
 		updateElementClassBackgroundTo('profession-icon', dark_background);
 
-		updateElementClassColorTo('status-bar', dark_status_color);
-		updateElementClassBackgroundTo('status-bar', dark_status_background_color);
-		updateElementClassBorderColorTo('status-bar', dark_status_color);
+		// updateElementClassColorTo('status-bar', dark_status_color);
+		// updateElementClassBackgroundTo('status-bar', dark_status_background_color);
+		// updateElementClassBorderColorTo('status-bar', dark_status_color);
 
 	} else if (currentTheme == 'light') {
-		toggleElementByClassName('portrait', false);
-		toggleElementByClassName('portrait-video', true);
+		// toggleElementByClassName('portrait', false);
+		// toggleElementByClassName('portrait-video', true);
 
 		updateElementColorTo('h1', light_h1);
 		updateElementColorTo('h3', light_h3);
@@ -245,9 +245,9 @@ function updateVisuals() {
 		updateElementClassColorTo('profession-icon', light_a);
 		updateElementClassBackgroundTo('profession-icon', light_background);
 
-		updateElementClassColorTo('status-bar', light_status_text_color);
-		updateElementClassBackgroundTo('status-bar', light_status_background_color);
-		updateElementClassBorderColorTo('status-bar', light_status_border_color);
+		// updateElementClassColorTo('status-bar', light_status_text_color);
+		// updateElementClassBackgroundTo('status-bar', light_status_background_color);
+		// updateElementClassBorderColorTo('status-bar', light_status_border_color);
 	}
 
 	updateElementClassColorTo('pageNumber', '#808080');

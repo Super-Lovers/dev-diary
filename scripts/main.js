9f51af68c58b3995c2e24fd3bd495d99e59147d0
Nikolay Ivanov <n.v.ivanov@st.hanze.nl>
2021-01-03 11:51:03 +0100

    easter egg

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-undef */
+
+console.log(
+	'\n\n,gggggggggggggggggggggggggggggggggggggggggggggggggggggwwwwwwwwwwwwwww=\n]$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$lllllllllllllllllllL\n]$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$lllllllllllllllllllllL\n]$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$l$$lllllllllllllllllllllllL\n|$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$lllllllllllllllllllllllllllllllL\n|$$$$$$$$$$$$$$$$$$$$$$$$$$&$llllllllllllllllllllllllllllllllllllllllL\n|$$$$$$$$$$$Ml$*MT|||TT|||||||||"|\'|i||\'\'"i&lllllllllllllllllllllll||L\n|%%$$$$lllW|||||||||||ll@WWy@l|lll|||||lllL|,||*llYlT|lllT***|lll\'lM`L\n||||l|||W|||||l|||l&lllllll||||||||||||||||| `||||||||||LL|||||\'\',kg"L\n]$@il|llllll|llll|llMlllLlll||l|||||||||||||||||||||||||||||||||j@g@M{\n]llllllllllllllllllllillllllL||LLlL|||l||||||lL||\'|||||||,|||||j@@@@@[\n|llllllL|l|||||llllllllllllLlllllllllL||||||||||||||||||||||gg@@@@@@@[\n||l|||l|||||||||l||||||||||||||||ll|lllLll||;|||||||||||,@@@@@@@@@@@@[\n|gL||||llll||||||iM$lllllllWWllllllMlllll|||||||||||,gg@@@@@@@@@@@@@@[\n]@@@@$@@@$$$$$@@%@@@@@@@$$$$$$$$@Ww|||||||||,,|,ygg@@@@@@@@@@@@@@@@@@[\n]@@@@@$$@@@@@@@@@@@$$@@@@@@@WW$ijl$llllll@@@$@@@@@@@@@@@@@@@@@@@@@@@@[\n]@@@@@@@@%@@@NNN%%NNNM%MMM$$$$%w$$lggg@@ggg@@@@@@@@@@@@@@@@@@@@@@@@@@[\n]@@@@@@@%%@@@@$$@@@@@@@@$$$$$$$$$@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@[\n]@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@$@@@@@@@@@@@@@@@@@@@@@@@@@[\n]@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@[\n]@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%@@@@@@@@@@@@@@@@@@@@@[\n\'""""""""""""""""""""""""""""""""""*****"***"""""^""""""""""""""""""*"\n\n\n\t\t\t\t\t\t\tC C  1 ( L C ,\n\t\t\t\t\t\t\t/\\\'mE r K. /\\\n\n\n'
+);
+
 const yearItems = document.getElementsByClassName('yearLabel');
 
 for (let i = 0; i < yearItems.length; i++) {
